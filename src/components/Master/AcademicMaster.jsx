5411354e2f760a0118720e7d4e3ca1f16a2e4cd9
Your Name <anandkumar2022bth@gmail.com>
2025-12-29 19:06:30 +0530

    React Tab addded

--- src/components/Master/AcademicMaster.jsx
+++ src/components/Master/AcademicMaster.jsx
@@ -17,13 +17,24 @@ import FeesPayment from './FeeMaster/FeesPayment'
 import Admission from '../Admission/Admission'
 import Registration from '../Registration/Registration'
 import StudentProfile from '../Student/StudentProfile'
+import ReactTabs from '../School/ReactTabs'
+import { Home, User, Settings, Mail, Bell, TrendingUp, Award, Heart } from 'lucide-react';
+import Permission from './Permission'
 
 const AcademicMaster = () => {
+    const tabs = [
+      { id: 'Organization', label: 'Organization', icon: Home, color: '#6f42c1', component: <OrganizationMaster/> },
+      { id: 'BankAccount', label: 'BankAccount', icon: User, color: '#0d6efd', component: <CreateBankAccount/> },
+      { id: 'User', label: 'User', icon: Mail, color: '#0dcaf0', component: <User/> },
+      { id: 'Permission', label: 'Permission', icon: Bell, color: '#ffc107', component: <Permission/> },
+      { id: 'Branch', label: 'Branch', icon: TrendingUp, color: '#198754', component: <Branch/> },
+      // { id: 'settings', label: 'Settings', icon: Settings, color: '#6c757d', component: <Subject/> },
+    ];
   return (
     <div>
-      <StudentProfile/>
+      {/* <StudentProfile/>
       <FeesPayment/>
-      <CreateAcademicYear/>a
+      <CreateAcademicYear/>
       <ClassMaster/>
       <SectionMaster/>
       <Subject/>
@@ -38,7 +49,8 @@ const AcademicMaster = () => {
       <CreateScholarship/>
       <CreateTax/>
       <Admission/>
-      <Registration/>
+      <Registration/> */}
+      <ReactTabs tabs={tabs} />
     </div>
   )
 }
