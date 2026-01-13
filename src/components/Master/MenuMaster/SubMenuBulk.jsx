e476e2ad8a56ac1cda0a3a6639f5cf5fd76ffcd1
Your Name <anandkumar2022bth@gmail.com>
2026-01-13 22:08:44 +0530

    submenu

--- src/components/Master/MenuMaster/SubMenuBulk.jsx
+++ src/components/Master/MenuMaster/SubMenuBulk.jsx
@@ -21,7 +21,7 @@ const SubMenuBulk = () => {
         name: "",
         code: "",
         pageUrl: "",
-        icon: "",
+        icon: { label: "", value: "" },
         displayOrder: "",
         branchId: null,
         orgId: "ORG001"
@@ -59,7 +59,7 @@ const SubMenuBulk = () => {
                 name: values.name,
                 code: values.code,
                 pageUrl: values.pageUrl,
-                icon: values.icon,
+                icon: values.icon?.value,
                 displayOrder: Number(values.displayOrder),
                 branchId: values.branchId?.value,
                 orgId: values.orgId
@@ -73,9 +73,9 @@ const SubMenuBulk = () => {
                 // handleGetSubMenus()
                 // setTableData(prev => [...prev, payload[0]]);
                 // setValues(initialData);
-               
+
                 notify(res?.message, "success");
-                 handleGetMenus(values.branchId?.value)
+                handleGetMenus(values.branchId?.value)
             } else {
                 notify(res?.message, "error");
             }
@@ -143,6 +143,64 @@ const SubMenuBulk = () => {
         // handleGetMenus()
         handleGetSubMenus()
     }, [])
+
+
+    const iconOptions = [
+  { value: "fa fa-cash-register", label: <i className="fa fa-cash-register" /> },
+  { value: "fa fa-money-bill", label: <i className="fa fa-money-bill" /> },
+  { value: "fa fa-credit-card", label: <i className="fa fa-credit-card" /> },
+  { value: "fa fa-wallet", label: <i className="fa fa-wallet" /> },
+  { value: "fa fa-university", label: <i className="fa fa-university" /> },
+
+  { value: "fa fa-school", label: <i className="fa fa-school" /> },
+  { value: "fa fa-book", label: <i className="fa fa-book" /> },
+  { value: "fa fa-book-open", label: <i className="fa fa-book-open" /> },
+  { value: "fa fa-user-graduate", label: <i className="fa fa-user-graduate" /> },
+  { value: "fa fa-chalkboard-teacher", label: <i className="fa fa-chalkboard-teacher" /> },
+
+  { value: "fa fa-users", label: <i className="fa fa-users" /> },
+  { value: "fa fa-user", label: <i className="fa fa-user" /> },
+  { value: "fa fa-id-card", label: <i className="fa fa-id-card" /> },
+  { value: "fa fa-address-card", label: <i className="fa fa-address-card" /> },
+  { value: "fa fa-user-circle", label: <i className="fa fa-user-circle" /> },
+
+  { value: "fa fa-calendar", label: <i className="fa fa-calendar" /> },
+  { value: "fa fa-calendar-alt", label: <i className="fa fa-calendar-alt" /> },
+  { value: "fa fa-clock", label: <i className="fa fa-clock" /> },
+  { value: "fa fa-bell", label: <i className="fa fa-bell" /> },
+  { value: "fa fa-bell-slash", label: <i className="fa fa-bell-slash" /> },
+
+  { value: "fa fa-cog", label: <i className="fa fa-cog" /> },
+  { value: "fa fa-tools", label: <i className="fa fa-tools" /> },
+  { value: "fa fa-wrench", label: <i className="fa fa-wrench" /> },
+  { value: "fa fa-sliders-h", label: <i className="fa fa-sliders-h" /> },
+  { value: "fa fa-database", label: <i className="fa fa-database" /> },
+
+  { value: "fa fa-file", label: <i className="fa fa-file" /> },
+  { value: "fa fa-file-alt", label: <i className="fa fa-file-alt" /> },
+  { value: "fa fa-file-excel", label: <i className="fa fa-file-excel" /> },
+  { value: "fa fa-file-pdf", label: <i className="fa fa-file-pdf" /> },
+  { value: "fa fa-upload", label: <i className="fa fa-upload" /> },
+
+  { value: "fa fa-download", label: <i className="fa fa-download" /> },
+  { value: "fa fa-print", label: <i className="fa fa-print" /> },
+  { value: "fa fa-eye", label: <i className="fa fa-eye" /> },
+  { value: "fa fa-edit", label: <i className="fa fa-edit" /> },
+  { value: "fa fa-trash", label: <i className="fa fa-trash" /> },
+
+  { value: "fa fa-check", label: <i className="fa fa-check" /> },
+  { value: "fa fa-times", label: <i className="fa fa-times" /> },
+  { value: "fa fa-plus", label: <i className="fa fa-plus" /> },
+  { value: "fa fa-minus", label: <i className="fa fa-minus" /> },
+  { value: "fa fa-search", label: <i className="fa fa-search" /> },
+
+  { value: "fa fa-chart-bar", label: <i className="fa fa-chart-bar" /> },
+  { value: "fa fa-chart-pie", label: <i className="fa fa-chart-pie" /> },
+  { value: "fa fa-chart-line", label: <i className="fa fa-chart-line" /> },
+  { value: "fa fa-home", label: <i className="fa fa-home" /> },
+  { value: "fa fa-dashboard", label: <i className="fa fa-dashboard" /> },
+];
+
     useEffect(() => {
         if (localData?.UserId) {
             dispatch(getEmployeeWise({
@@ -211,7 +269,7 @@ const SubMenuBulk = () => {
                         className="form-control"
                     />
 
-                    <Input
+                    {/* <Input
                         type="text"
                         name="icon"
                         value={values.icon}
@@ -219,6 +277,15 @@ const SubMenuBulk = () => {
                         respclass="col-xl-2 col-md-4 col-sm-6 col-12"
                         onChange={handleChange}
                         className="form-control"
+                    /> */}
+
+                    <ReactSelect
+                        placeholderName="icon"
+                        respclass="col-xl-2 col-md-4 col-sm-6 col-12"
+                        name="icon"
+                        dynamicOptions={iconOptions}
+                        handleChange={handleSelect}
+                        value={values.icon}
                     />
 
                     <Input
